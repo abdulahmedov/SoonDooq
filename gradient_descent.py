186f990d7e6b0e33d62f2a5b1809344a77fd7ea2
abdulahmedov <abdulahmedov@gmail.com>
2019-11-06 16:08:05 +0300

    Create gradient_descent.py

---
+++ gradient_descent.py
@@ -0,0 +1,138 @@
+def  cal_cost(theta,X,y):
+    '''
+    
+    Calculates the cost for given X and Y. The following shows and example of a single dimensional X
+    theta = Vector of thetas 
+    X     = Row of X's np.zeros((2,j))
+    y     = Actual y's np.zeros((2,1))
+    
+    where:
+        j is the no of features
+    '''
+    
+    m = len(y)
+    
+    predictions = X.dot(theta)
+    cost = (1/2*m) * np.sum(np.square(predictions-y))
+    return cost
+
+
+def gradient_descent(X,y,theta,learning_rate=0.01,iterations=100):
+    '''
+    X    = Matrix of X with added bias units
+    y    = Vector of Y
+    theta=Vector of thetas np.random.randn(j,1)
+    learning_rate 
+    iterations = no of iterations
+    
+    Returns the final theta vector and array of cost history over no of iterations
+    '''
+    m = len(y)
+    cost_history = np.zeros(iterations)
+    theta_history = np.zeros((iterations,2))
+    for it in range(iterations):
+        
+        prediction = np.dot(X,theta)
+        
+        theta = theta -(1/m)*learning_rate*( X.T.dot((prediction - y)))
+        theta_history[it,:] =theta.T
+        cost_history[it]  = cal_cost(theta,X,y)
+        
+    return theta, cost_history, theta_history
+    
+    
+def stocashtic_gradient_descent(X,y,theta,learning_rate=0.01,iterations=10):
+    '''
+    X    = Matrix of X with added bias units
+    y    = Vector of Y
+    theta=Vector of thetas np.random.randn(j,1)
+    learning_rate 
+    iterations = no of iterations
+    
+    Returns the final theta vector and array of cost history over no of iterations
+    '''
+    m = len(y)
+    cost_history = np.zeros(iterations)
+    
+    
+    for it in range(iterations):
+        cost =0.0
+        for i in range(m):
+            rand_ind = np.random.randint(0,m)
+            X_i = X[rand_ind,:].reshape(1,X.shape[1])
+            y_i = y[rand_ind].reshape(1,1)
+            prediction = np.dot(X_i,theta)
+
+            theta = theta -(1/m)*learning_rate*( X_i.T.dot((prediction - y_i)))
+            cost += cal_cost(theta,X_i,y_i)
+        cost_history[it]  = cost
+        
+    return theta, cost_history
+    
+    
+def minibatch_gradient_descent(X,y,theta,learning_rate=0.01,iterations=10,batch_size =20):
+    '''
+    X    = Matrix of X without added bias units
+    y    = Vector of Y
+    theta=Vector of thetas np.random.randn(j,1)
+    learning_rate 
+    iterations = no of iterations
+    
+    Returns the final theta vector and array of cost history over no of iterations
+    '''
+    m = len(y)
+    cost_history = np.zeros(iterations)
+    n_batches = int(m/batch_size)
+    
+    for it in range(iterations):
+        cost =0.0
+        indices = np.random.permutation(m)
+        X = X[indices]
+        y = y[indices]
+        for i in range(0,m,batch_size):
+            X_i = X[i:i+batch_size]
+            y_i = y[i:i+batch_size]
+            
+            X_i = np.c_[np.ones(len(X_i)),X_i]
+           
+            prediction = np.dot(X_i,theta)
+
+            theta = theta -(1/m)*learning_rate*( X_i.T.dot((prediction - y_i)))
+            cost += cal_cost(theta,X_i,y_i)
+        cost_history[it]  = cost
+        
+    return theta, cost_history
+    
+    
+def plot_GD(n_iter,lr,ax,ax1=None):
+     """
+     n_iter = no of iterations
+     lr = Learning Rate
+     ax = Axis to plot the Gradient Descent
+     ax1 = Axis to plot cost_history vs Iterations plot
+
+     """
+     _ = ax.plot(X,y,'b.')
+     theta = np.random.randn(2,1)
+
+     tr =0.1
+     cost_history = np.zeros(n_iter)
+     for i in range(n_iter):
+        pred_prev = X_b.dot(theta)
+        theta,h,_ = gradient_descent(X_b,y,theta,lr,1)
+        pred = X_b.dot(theta)
+
+        cost_history[i] = h[0]
+
+        if ((i % 25 == 0) ):
+            _ = ax.plot(X,pred,'r-',alpha=tr)
+            if tr < 0.8:
+                tr = tr+0.2
+     if not ax1== None:
+        _ = ax1.plot(range(n_iter),cost_history,'b.')
+    
+    
+    
+    
+    
+    
